60d7678d4e20a9da2816e599b9192f63bc0df02d
belgrades <fcremaldc@gmail.com>
2017-07-13 17:47:38 +0200

    Changing scripts structure

--- backfitting.R
+++ backfitting.R
@@ -1,7 +1,18 @@
 
 # Load data ---------------------------------------------------------------
 
-load(file = "data/ore.RData")
+# Generating training data
+n = 150; p = 200
+X = 0.5*matrix(runif(n*p),n,p) + matrix(rep(0.5*runif(n),p),n,p)
+# Generating response
+y = -2*sin(X[,1]) + X[,2]^2-1/3 + X[,3]-1/2 + exp(-X[,4]) + exp(-1)-1
+
+# Test data ---------------------------------------------------------------
+
+n = 500; d = 200
+X.te = 0.5*matrix(runif(n*d),n,d) + matrix(rep(0.5*runif(n),d),n,d)
+# Generating response
+y.te = -2*sin(X.te[,1]) + X.te[,2]^2-1/3 + X.te[,3]-1/2 + exp(-X.te[,4]) + exp(-1)-1
 
 # Rename variable because Giulia is so fucking picky ----------------------
 
@@ -22,7 +33,7 @@ f = matrix(data = rep(0, n*p), nrow = n, ncol = p)
 
 # Set and rss0 rss -----------------------------------------------------------------
 
-rss_0 = sum((y - alpha - apply(f, 1, sum))^2)
+rss0 = sum((y - alpha - apply(f, 1, sum))^2)
 rss = 0
 
 # Day by day there's a man in a suit that's gonna make you pay
@@ -38,7 +49,7 @@ rsss = numeric()
 while(abs(rss0-rss) > tol && iter <= max_iter){
   rss = rss0
   iter = iter + 1
-
+  
   for(j in 1:p){
     r_j = y - alpha - apply(f, 1, sum) + f[, j]
     f[, j] = predict(smooth.spline(x = X[, j], y = r_j), X[, j])$y
